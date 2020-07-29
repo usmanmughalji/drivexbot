11a9bad1f6a75952edcb6b26ec50a67170f32fd5
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-07-29 21:29:25 +0500

    New Welcome Message
    introduced the new welcome function for more ease

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -37,8 +37,13 @@ from tobrot import (
 
 from pyrogram import Client, Filters, MessageHandler, CallbackQueryHandler
 
+<<<<<<< HEAD
 from tobrot.plugins.new_join_fn import new_join_f, help_message_f, rename_message_f
 from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f, incoming_purge_message_f, incoming_gdrive_message_f, g_yt_playlist
+=======
+from tobrot.plugins.new_join_fn import new_join_f, welcome_message_f, help_message_f, rename_message_f
+from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f, incoming_purge_message_f, incoming_gdrive_message_f
+>>>>>>> 71c6e99... New Welcome Message
 from tobrot.plugins.rclone_size import check_size_g, g_clearme
 from tobrot.plugins.status_message_fn import (
     status_message_f,
@@ -167,7 +172,7 @@ if __name__ == "__main__" :
     app.add_handler(new_join_handler)
     #
     group_new_join_handler = MessageHandler(
-        help_message_f,
+        welcome_message_f,
         filters=Filters.chat(chats=AUTH_CHANNEL) & Filters.new_chat_members
     )
     app.add_handler(group_new_join_handler)
