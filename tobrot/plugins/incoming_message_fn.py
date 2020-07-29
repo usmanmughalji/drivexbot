e40f0c6130b937776cb925a642a6389dd37580a1
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-07-29 21:48:13 +0500

    Changed Error Message
    little bit changes :)

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -96,7 +96,7 @@ async def incoming_message_f(client, message):
             await i_m_sefg.edit_text(err_message)
     else:
         await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            "**Aww**! What you have entered 🥺 \nPlease type `/help` \n"
             f"<b>API Error</b>: {cf_name}"
         )
 #
@@ -151,7 +151,7 @@ async def incoming_gdrive_message_f(client, message):
         )
     else:
         await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            "**Aww**! What you have entered 🥺 \nPlease type `/help` \n"
             f"<b>API Error</b>: {cf_name}"
         )
 
@@ -206,7 +206,7 @@ async def incoming_youtube_dl_f(client, message):
             )
     else:
         await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
+            "**Aww**! What you have entered 🥺 \nPlease type `/help` \n"
             f"<b>API Error</b>: {cf_name}"
         )
 #playlist
