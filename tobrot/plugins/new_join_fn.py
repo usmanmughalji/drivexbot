5203be4dc1f4336976ed142cad01b6cc367b8a99
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2020-08-14 04:12:55 +0500

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -33,16 +33,16 @@ async def new_join_f(client, message):
 
     
 async def welcome_message_f(client, message):
-    # await message.reply_text("no one gonna help you ?¤£?¤£?¤£?¤£", quote=True)
+    # await message.reply_text("no one gonna help you ðŸ¤£ðŸ¤£ðŸ¤£ðŸ¤£", quote=True)
     #channel_id = str(AUTH_CHANNEL)[4:]
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""Welcome to Team Drive Ultimate X""", disable_web_page_preview=True)    
+    await message.reply_text("""Welcome to Team Drive X""", disable_web_page_preview=True)    
     
 
 async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you ?¤£?¤£?¤£?¤£", quote=True)
+    # await message.reply_text("no one gonna help you ðŸ¤£ðŸ¤£ðŸ¤£ðŸ¤£", quote=True)
     #channel_id = str(AUTH_CHANNEL)[4:]
     #message_id = 99
     # display the /help
